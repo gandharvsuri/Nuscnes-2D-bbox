d3abf6e295adfdba9456f3cecf9d6f9f8749608f
gandharv <gandharv.suri@iiitb.org>
2021-04-25 15:21:44 +0530

    first commit

---
+++ Preprocessing/dataprep.py
@@ -0,0 +1,57 @@
+import pandas as pd
+import numpy as np
+
+sample_data = pd.read_json("../Nuscenes Data/nuimages-v1.0-all-metadata/v1.0-train/sample_data.json")
+category = pd.read_json("../Nuscenes Data/nuimages-v1.0-all-metadata/v1.0-train/category.json")
+category["index"] = list(range(len(category)))
+category_index = dict(zip(category["token"],category["index"]))
+
+object_ann = pd.read_json("../Nuscenes Data/nuimages-v1.0-all-metadata/v1.0-train/object_ann.json")
+
+
+for i, row in sample_data.iterrows():
+    filename = row["filename"].split("/")
+
+    if filename[1] == "CAM_FRONT":
+        fname = filename[2].replace("."+row["fileformat"], ".txt")
+        path =  "../Nuscenes Data/labels/tests/" + fname
+        print(fname)
+        f = open(path, "w")
+
+        object_df = object_ann[object_ann["sample_data_token"] == row["token"]]
+        flag = len(object_df)
+        bbox = object_df["bbox"].values
+        
+        classes = object_df["category_token"].values
+        classes = [category_index[token] for token in classes]
+        x_center = []
+        y_center = []
+        width = []
+        height = []
+
+        for x1,y1,x2,y2 in bbox:
+            x_center.append(np.mean([x1,x2]))
+            y_center.append(np.mean([y1,y2]))
+            width.append(abs(x1-x2))
+            height.append(abs(y1-y2))
+
+        x_center = np.array(x_center)/1600
+        y_center = np.array(y_center)/900
+        width = np.array(width)/1600
+        height = np.array(height)/900
+
+        assert len(classes) == len(x_center)
+        assert len(x_center) == len(y_center)
+        assert len(y_center) == len(width)
+        assert len(width) == len(height)
+
+        label = list(zip(classes,x_center,y_center,width,height))
+
+        for c,x,y,w,h in label:
+            f.write("{} {} {} {} {} ".format(c,x,y,w,h))
+        
+        f.close()
+        
+        if(flag > 0):
+            print(path)
+            break
